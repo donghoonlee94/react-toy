6d510202061561a0a58cd855a89231901e1d2014
donghoonlee94 <94donghoon.lee@gmail.com>
2021-09-15 21:02:34 +0900

    Add routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-// import messagesRoute from './routes/messages.js'
+import messagesRoute from './routes/messages.js'
 // import usersRoute from './routes/users.js'
 
 const app = express()
@@ -14,10 +14,10 @@ app.use(
   }),
 )
 
-// const routes = [...messagesRoute, ...usersRoute]
-// routes.forEach(({ method, route, handler }) => {
-//   app[method](route, handler)
-// })
+const routes = [...messagesRoute]
+routes.forEach(({ method, route, handler }) => {
+  app[method](route, handler)
+})
 
 app.listen(8000, () => {
   console.log('server listening on 8000...')
